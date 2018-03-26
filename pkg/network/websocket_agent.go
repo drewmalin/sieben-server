68db042d7aa85b7e12fbed9e17df672863e72251
Drew Malin <drew.malin@gmail.com>
2018-03-25 17:00:20 -0700

    beginnings of web socket server

---
+++ pkg/network/websocket_agent.go
@@ -0,0 +1,99 @@
+package network
+
+import (
+	"bytes"
+	"fmt"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/gorilla/websocket"
+	"github.com/sieben-server/pkg/logger"
+)
+
+const (
+	// Time allowed to write a message to the peer.
+	writeWait = 10 * time.Second
+
+	// Time allowed to read the next pong message from the peer.
+	pongWait = 60 * time.Second
+
+	// Send pings to peer with this period. Must be less than pongWait.
+	pingPeriod = (pongWait * 9) / 10
+
+	// Maximum message size allowed from peer.
+	maxMessageSize = 512
+)
+
+var (
+	newline = []byte{'\n'}
+	space   = []byte{' '}
+)
+
+// WebSocketAgent represents an individual web socket connection to a unique client.
+type WebSocketAgent struct {
+	id   string
+	conn *websocket.Conn
+}
+
+// NewWebSocketAgent instantiates a new WebSocketAgent.
+func NewWebSocketAgent(conn *websocket.Conn) *WebSocketAgent {
+	id, err := uuid.NewUUID()
+	if err != nil {
+		panic(err)
+	}
+	agent := &WebSocketAgent{
+		id:   id.String(),
+		conn: conn,
+	}
+	agent.conn.SetReadLimit(maxMessageSize)
+	agent.conn.SetReadDeadline(time.Now().Add(pongWait))
+	agent.conn.SetPongHandler(func(string) error {
+		agent.conn.SetReadDeadline(time.Now().Add(pongWait))
+		return nil
+	})
+	return agent
+}
+
+func (agent *WebSocketAgent) String() string {
+	return fmt.Sprintf("(%s - %s)", agent.id, agent.conn.RemoteAddr().String())
+}
+
+// Run is the business.
+func (agent *WebSocketAgent) Run(server Server) {
+	for {
+		_, message, err := agent.conn.ReadMessage()
+		if err != nil {
+			logger.Get().Printf("Agent %s disconnected by client", agent.String())
+			server.OnClosedAgent(agent)
+			break
+		}
+		message = bytes.TrimSpace(bytes.Replace(message, newline, space, -1))
+		server.OnReceivePacket(&Packet{agent.id, message})
+	}
+}
+
+// Read reads data from this agent's connection into the specified byte array.
+func (agent *WebSocketAgent) Read(packet *Packet) (int, error) {
+	//return agent.conn.Read(packet.Data)
+	return 0, nil
+}
+
+// Write writes data into this agent's connection from the specified byte array.
+func (agent *WebSocketAgent) Write(packet *Packet) (int, error) {
+	if packet.SenderID != agent.id {
+		agent.conn.SetWriteDeadline(time.Now().Add(writeWait))
+		writer, err := agent.conn.NextWriter(websocket.TextMessage)
+		if err != nil {
+			panic(err)
+		}
+		defer writer.Close()
+
+		return writer.Write(packet.Data)
+	}
+	return 0, nil
+}
+
+// Close closes this agent's connection.
+func (agent *WebSocketAgent) Close() error {
+	return agent.conn.Close()
+}
---
+++ pkg/network/websocket_server.go
@@ -0,0 +1,118 @@
+package network
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+	"github.com/sieben-server/pkg/logger"
+)
+
+// WebSocketServer is a Server which handles its connections via web sockets.
+type WebSocketServer struct {
+	port           int
+	maxConnections int
+	agentSet       *AgentSet
+
+	newConnChannel   chan *websocket.Conn
+	deadAgentChannel chan Agent
+	packetChannel    chan *Packet
+}
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
+// NewWebSocketServer instantiates a new WebSocketServer.
+func NewWebSocketServer(port int, maxConnections int) *WebSocketServer {
+	agentSet := NewAgentSet()
+	newConnChannel := make(chan *websocket.Conn, maxConnections)
+	deadAgentChannel := make(chan Agent, maxConnections)
+	packetChannel := make(chan *Packet, maxConnections)
+
+	return &WebSocketServer{
+		port:             port,
+		maxConnections:   maxConnections,
+		agentSet:         agentSet,
+		newConnChannel:   newConnChannel,
+		deadAgentChannel: deadAgentChannel,
+		packetChannel:    packetChannel,
+	}
+}
+
+// Start informs this server to begin listening on its port. New connections will be passed to new agents for handling,
+// and the agent lifecycle will be managed. Start will not return.
+func (server *WebSocketServer) Start() {
+	logger.Get().Printf("Web Socket Server startup on port: %d, maximum allowed connections: %d", server.port, server.maxConnections)
+
+	go server.handleNewConnections()
+
+	for {
+		select {
+		case conn := <-server.newConnChannel:
+			webSocketAgent := NewWebSocketAgent(conn)
+			server.agentSet.Add(webSocketAgent)
+			logger.Get().Printf("New agent: %s", webSocketAgent.String())
+			server.logStatus()
+
+			go webSocketAgent.Run(server)
+
+		case agent := <-server.deadAgentChannel:
+			server.agentSet.Remove(agent)
+			agent.Close()
+
+			logger.Get().Printf("Agent %s closed", agent.String())
+			server.logStatus()
+
+		case packet := <-server.packetChannel:
+			for agent := range server.agentSet.Agents {
+				agent.Write(packet)
+			}
+		}
+	}
+}
+
+func (server *WebSocketServer) handleNewConnections() {
+	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+		conn, err := upgrader.Upgrade(writer, request, nil)
+		if err != nil {
+			logger.Get().Println(err)
+			panic(err)
+		}
+		if server.agentSet.Size() >= server.maxConnections {
+			logger.Get().Println("Connection attempted while agent count is at capacity. Connection will be closed.")
+			conn.Close()
+		} else {
+			server.newConnChannel <- conn
+		}
+	})
+	err := http.ListenAndServe(fmt.Sprintf(":%d", server.port), nil)
+	if err != nil {
+		panic(err)
+	}
+}
+
+// OnClosedAgent handles the closure of an agent's connection.
+func (server *WebSocketServer) OnClosedAgent(agent Agent) {
+	server.deadAgentChannel <- agent
+}
+
+// OnReceivePacket handles the receipt of a new packet.
+func (server *WebSocketServer) OnReceivePacket(packet *Packet) {
+	server.packetChannel <- packet
+}
+
+// Stop closes all agents associated with this server.
+func (server *WebSocketServer) Stop() {
+	for agent := range server.agentSet.Agents {
+		agent.Close()
+	}
+}
+
+func (server *WebSocketServer) logStatus() {
+	logger.Get().Printf("Current agent count: %d / %d", server.agentSet.Size(), server.maxConnections)
+}
